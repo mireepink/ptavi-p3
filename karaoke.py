1359945056f12fe26b0508c11a52d41e9fa6d595
mireya elisabet valenzuela carballo <mireya@iota22.aulas.gsyc.es>
2014-10-13 13:33:00 +0200

    Añadido karaoke.py

---
+++ karaoke.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+import sys
+from smallsmilhandler import SmallSMILHandler
+
+
+class Karaoke(SmallSMILHandler):
+
+    def __str__(self):
+        salida = ""
+        for diccionario in self.Lista:
+            salida += str('\n' + diccionario['etiqueta'] + '\t')
+            for clave in diccionario:
+                if clave != 'etiqueta' and diccionario[clave] != 'Null':
+                  salida += str(clave + '="' + diccionario[clave] + '"\t')
+        return salida
+        
+if __name__ == "__main__":
+
+    try:
+        fichero = sys.argv[1]   
+    except IndexError:
+        sys.exit('Usage: python karaoke.py file.smil.')
+
+parser = make_parser()
+sHandler = Karaoke()
+parser.setContentHandler(sHandler)
+parser.parse(open(fichero))
+print sHandler
